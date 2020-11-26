c7ff4e776dc0b4709a90626900e7fc9af6141fed
Gunay-Jafar <cafarligunay06@gmail.com>
2020-11-26 20:28:38 +0400

    Added Swagger Configuration

--- src/main/java/web/StoreProject/StoreProjectApplication.java
+++ src/main/java/web/StoreProject/StoreProjectApplication.java
@@ -3,10 +3,7 @@ package web.StoreProject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import web.StoreProject.entity.Customer;
+
 
 @SpringBootApplication
 public class StoreProjectApplication  implements CommandLineRunner{
@@ -17,6 +14,7 @@ public class StoreProjectApplication  implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
+
 
     }
 }
--- src/main/java/web/StoreProject/entity/Customer.java
+++ src/main/java/web/StoreProject/entity/Customer.java
@@ -16,6 +16,7 @@ import javax.persistence.Id;
 @Data
 @ToString
 public class Customer {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
